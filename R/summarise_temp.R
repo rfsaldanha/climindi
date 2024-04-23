fae5bb57c92f58229bfa4365b12c27c23048c821
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-23 21:59:30 +0200

    Fix global bindings.

--- R/summarise_temp.R
+++ R/summarise_temp.R
@@ -1,3 +1,4 @@
+#' @importFrom rlang .data
 summarise_temp <- function(.x, var){
   # Assertions
   checkmate::assert_data_frame(x = .x)
@@ -12,17 +13,17 @@ summarise_temp <- function(.x, var){
     dplyr::summarise(
       n = dplyr::n(),
       avg = mean({{var}}, na.rm = TRUE),
-      sd = sd({{var}}, na.rm = TRUE),
-      se = sd/sqrt(n),
+      sd = stats::sd({{var}}, na.rm = TRUE),
+      se = .data[["sd"]]/sqrt(.data[["n"]]),
       max = max({{var}}, na.rm = TRUE),
       min = min({{var}}, na.rm = TRUE),
-      days_a1 = sum({{var}} > avg + (1 * sd), na.rm = TRUE),
-      days_a2 = sum({{var}} > avg + (2 * sd), na.rm = TRUE),
-      days_b1 = sum({{var}} < avg - (1 * sd), na.rm = TRUE),
-      days_b2 = sum({{var}} < avg - (2 * sd), na.rm = TRUE),
-      days_c1 = nseq::trle_cond({{var}}, a = 3, b = avg),
-      days_c2 = nseq::trle_cond({{var}}, a = 5, b = avg),
-      days_d1 = nseq::trle_cond({{var}}, a = 3, b_op = "lte", b = avg),
-      days_d2 = nseq::trle_cond({{var}}, a = 5, b_op = "lte", b = avg)
+      days_a1 = sum({{var}} > .data[["avg"]] + (1 * .data[["sd"]]), na.rm = TRUE),
+      days_a2 = sum({{var}} > .data[["avg"]] + (2 * .data[["sd"]]), na.rm = TRUE),
+      days_b1 = sum({{var}} < .data[["avg"]] - (1 * .data[["sd"]]), na.rm = TRUE),
+      days_b2 = sum({{var}} < .data[["avg"]] - (2 * .data[["sd"]]), na.rm = TRUE),
+      days_c1 = nseq::trle_cond({{var}}, a = 3, b = .data[["avg"]]),
+      days_c2 = nseq::trle_cond({{var}}, a = 5, b = .data[["avg"]]),
+      days_d1 = nseq::trle_cond({{var}}, a = 3, b_op = "lte", b = .data[["avg"]]),
+      days_d2 = nseq::trle_cond({{var}}, a = 5, b_op = "lte", b = .data[["avg"]])
     )
 }
