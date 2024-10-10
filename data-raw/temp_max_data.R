6d79f9284a4f239de70a80d76410bb866bbc7f9e
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-10 09:36:39 +0200

    Fix data

--- data-raw/temp_max_data.R
+++ data-raw/temp_max_data.R
@@ -9,7 +9,7 @@ temp_max_data <- zendown::zen_file(13906834, "Tmax_3.2.3.parquet") |>
   filter(code_muni %in% c(3304557, 3136702, 3303401, 3106200)) |>
   filter(name == "Tmax_3.2.3_mean") |>
   filter(date >= as.Date("1961-01-01")) |>
-  filter(date <= as.Date("2023-01-01")) |>
+  filter(date <= as.Date("2023-12-31")) |>
   select(-name) |>
   collect()
 
