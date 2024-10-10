970706971483e3dd316e9a61bb412f60a0d09e14
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-10 10:32:13 +0200

    Rework max temp indicators with monthly normals

--- R/summarise_temp_max.R
+++ R/summarise_temp_max.R
@@ -3,6 +3,8 @@
 #' The function computes maximum temperature indicators from grouped data. Expects temperature in celsius degrees.
 #' 
 #' @details
+#' The heat waves indicators are computed based on climatological normals, created with the `summarise_normal()` function and passed with the `normals_df` argument. Keys to join the normals data must be present (like id, year, and month)  and use the same names.
+#' 
 #' The following indicators are computed for each group.
 #' \itemize{
 #'  \item{`count` Count of data points}
@@ -17,9 +19,8 @@
 #'  \item{`p25` 25th percentile}
 #'  \item{`p75` 75th percentile}
 #'  \item{`p90` 10th percentile}
-#'  \item{`p10_w` 10th percentile from a rolling window of size 5}
-#'  \item{`p90_w` 90th percentile from a rolling window of size 5}
-#'  \item{`heat_waves` Count of heat waves occurences, 5 or more consecutive days with maximum temperature above the climatological normal value plus 5 celsius degrees}
+#'  \item{`heat_waves_3d` Count of heat waves occurences, with 3 or more consecutive days with maximum temperature above the climatological normal value plus 5 celsius degrees}
+#'  \item{`heat_waves_5d` Count of heat waves occurences, with 5 or more consecutive days with maximum temperature above the climatological normal value plus 5 celsius degrees}
 #'  \item{`tx90p` Count of warm days, days with maximum temperatures above the 90th percentile}
 #'  \item{`t_25` Count of days with temperatures above or equal to 25 celsius degrees}
 #'  \item{`t_30` Count of days with temperatures above or equal to 30 celsius degrees}
@@ -30,30 +31,37 @@
 #' @param .x grouped data, created with `dplyr::group_by()`
 #' @param value_var name of the variable with temperature values.
 #' @param normals_df normals data, created with `summarise_normal()`
-#' @param id_var id variable to join normals with temperature data
 #'
 #' @return A tibble.
 #' @export
 #' @importFrom rlang .data
 #'
 #' @examples
-#' # Compute normals
+#' # Compute monthly normals
 #' normals <- temp_max_data |>
-#'   dplyr::group_by(code_muni) |>
+#'   # Identify year
+#'   dplyr::mutate(year = lubridate::year(date)) |>
+#'   # Identify month
+#'   dplyr::mutate(month = lubridate::month(date)) |>
+#'   # Group by id variable, year and month
+#'   dplyr::group_by(code_muni, year, month) |>
 #'   summarise_normal(date_var = date, value_var = value, year_start = 1961, year_end = 1990) |>
 #'   dplyr::ungroup()
 #' 
+#' # Compute indicators
 #' temp_max_data |>
 #'  # Identify year
 #'  dplyr::mutate(year = lubridate::year(date)) |>
-#'  # Group by municipality code and year
-#'  dplyr::group_by(code_muni, year) |>
-#'  # Compute temperature indicators
-#'  summarise_temp_max(value_var = value, normals_df = normals, id_var = "code_muni") |>
+#'  # Identify month
+#'  dplyr::mutate(month = lubridate::month(date)) |>
+#'  # Group by id variable, year and month
+#'  dplyr::group_by(code_muni, year, month) |>
+#'  # Compute maximum temperature indicators
+#'  summarise_temp_max(value_var = value, normals_df = normals) |>
 #'  # Ungroup
 #'  dplyr::ungroup()
 #' 
-summarise_temp_max <- function(.x, value_var, normals_df, id_var){
+summarise_temp_max <- function(.x, value_var, normals_df){
   # Assertions
   checkmate::assert_data_frame(x = .x)
 
@@ -63,8 +71,9 @@ summarise_temp_max <- function(.x, value_var, normals_df, id_var){
   )
 
   # Compute indicators
-  .x |>
-    dplyr::inner_join(normals_df, by = {{id_var}}) |>
+  suppressMessages(
+    .x |>
+    dplyr::inner_join(normals_df) |>
     dplyr::summarise(
       count = dplyr::n(),
       normal = mean(.data[["normal"]], na.rm = TRUE),
@@ -78,15 +87,18 @@ summarise_temp_max <- function(.x, value_var, normals_df, id_var){
       p_25 = stats::quantile({{value_var}}, probs = 0.25, names = FALSE),
       p_75 = stats::quantile({{value_var}}, probs = 0.75, names = FALSE),
       p_90 = stats::quantile({{value_var}}, probs = 0.90, names = FALSE),
-      p10_w = caTools::runquantile({{value_var}}, k = 5, p = 0.1)[1],
-      p90_w = caTools::runquantile({{value_var}}, k = 5, p = 0.9)[1],
-      heat_waves = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]] + 5, b_op = "gte"),
-      tx90p = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = .data[["p90_w"]], b_op = "gte"),
+      #p10_w = caTools::runquantile({{value_var}}, k = 5, p = 0.1)[1],
+      #p90_w = caTools::runquantile({{value_var}}, k = 5, p = 0.9)[1],
+      heat_waves_3d = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal"]] + 5, b_op = "gte"),
+      heat_waves_5d = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]] + 5, b_op = "gte"),
+      tx90p = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = .data[["p_90"]], b_op = "gte"),
       t_25 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 25, b_op = "gte"),
       t_30 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 30, b_op = "gte"),
       t_35 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 35, b_op = "gte"),
       t_40 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 40, b_op = "gte"),
     )
+  )
+  
 }
 
 
--- tests/testthat/test-summarise_temp_max.R
+++ tests/testthat/test-summarise_temp_max.R
@@ -1,6 +1,8 @@
 test_that("summarise_temp works", {
   normals <- temp_max_data |>
-    dplyr::group_by(code_muni) |>
+    dplyr::mutate(year = lubridate::year(date)) |>
+    dplyr::mutate(month = lubridate::month(date)) |>
+    dplyr::group_by(code_muni, year, month) |>
     summarise_normal(date_var = date, value_var = value, year_start = 1961, year_end = 1990) |>
     dplyr::ungroup()
   
@@ -8,10 +10,10 @@ test_that("summarise_temp works", {
     dplyr::mutate(year = lubridate::year(date)) |>
     dplyr::mutate(month = lubridate::month(date)) |>
     dplyr::group_by(code_muni, year, month) |>
-    summarise_temp_max(value_var = value, normals_df = normals, id_var = "code_muni") |>
+    summarise_temp_max(value_var = value, normals_df = normals) |>
     dplyr::ungroup()
 
-  expect_true(all(c("heat_waves", "tx90p") %in% names(res)))
+  expect_true(all(c("heat_waves_5d", "tx90p") %in% names(res)))
 })
 
 test_that("summarise_temp not works with ungrouped data", {
