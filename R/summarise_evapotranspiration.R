e5805aa9d28584beaf3093043a0b62ad621f93be
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-10 14:55:07 +0200

    Add evapotranspiration summarize function

---
+++ R/summarise_evapotranspiration.R
@@ -0,0 +1,95 @@
+#' Compute evapotranspirations indicators from grouped data
+#'
+#' The function computes evapotranspirations (ETo) indicators from grouped data. Expects solar radiation in millimeters (mm).
+#' 
+#' @details
+#' The high and low ETo indicators are computed based on climatological normals, created with the `summarise_normal()` function and passed with the `normals_df` argument. Keys to join the normals data must be present (like id, year, and month)  and use the same names.
+#' 
+#' The following indicators are computed for each group.
+#' \itemize{
+#'  \item{`count` Count of data points}
+#'  \item{`mean` Average}
+#'  \item{`median` Median}
+#'  \item{`sd` Standard deviation}
+#'  \item{`se` Standard error}
+#'  \item{`max` Maximum value}
+#'  \item{`min` Minimum value}
+#'  \item{`p10` 10th percentile}
+#'  \item{`p25` 25th percentile}
+#'  \item{`p75` 75th percentile}
+#'  \item{`p90` 90th percentile}
+#'  \item{`l_eto_3` Count of sequences of 3 days or more with evapotranspirations bellow the climatological normal}
+#'  \item{`l_eto_5` Count of sequences of 5 days or more with evapotranspirations bellow the climatological normal}
+#'  \item{`h_eto_3` Count of sequences of 3 days or more with evapotranspirations above the climatological normal}
+#'  \item{`h_eto_5` Count of sequences of 5 days or more with evapotranspirations above the climatological normal}
+#' }
+#' 
+#' @param .x grouped data, created with `dplyr::group_by()`
+#' @param value_var name of the variable with temperature values.
+#' @param normals_df normals data, created with `summarise_normal()`
+#'
+#' @return A tibble.
+#' @export
+#' @importFrom rlang .data
+#'
+#' @examples
+#' # Compute monthly normals
+#' normals <- evapotranspiration_data |>
+#'   # Identify year
+#'   dplyr::mutate(year = lubridate::year(date)) |>
+#'   # Identify month
+#'   dplyr::mutate(month = lubridate::month(date)) |>
+#'   # Group by id variable, year and month
+#'   dplyr::group_by(code_muni, year, month) |>
+#'   summarise_normal(date_var = date, value_var = value, year_start = 1961, year_end = 1990) |>
+#'   dplyr::ungroup()
+#' 
+#' # Compute indicators
+#' evapotranspiration_data |>
+#'  # Identify year
+#'  dplyr::mutate(year = lubridate::year(date)) |>
+#'  # Identify month
+#'  dplyr::mutate(month = lubridate::month(date)) |>
+#'  # Group by id variable, year and month
+#'  dplyr::group_by(code_muni, year, month) |>
+#'  # Compute evapotranspiration indicators
+#'  summarise_evapotrapiration(value_var = value, normals_df = normals) |>
+#'  # Ungroup
+#'  dplyr::ungroup()
+#' 
+summarise_evapotrapiration <- function(.x, value_var, normals_df){
+  # Assertions
+  checkmate::assert_data_frame(x = .x)
+
+  # Assert group
+  if(!dplyr::is_grouped_df(.x))(
+    stop(".x must be a grouped data frame")
+  )
+
+  # Compute indicators
+  suppressMessages(
+    .x |>
+    dplyr::inner_join(normals_df) |>
+    dplyr::summarise(
+      count = dplyr::n(),
+      normal = mean(.data[["normal"]], na.rm = TRUE),
+      mean = mean({{value_var}}, na.rm = TRUE),
+      median = stats::median({{value_var}}, na.rm = TRUE),
+      sd = stats::sd({{value_var}}, na.rm = TRUE),
+      se = .data[["sd"]]/sqrt(.data[["count"]]),
+      max = max({{value_var}}, na.rm = TRUE),
+      min = min({{value_var}}, na.rm = TRUE),
+      p10 = stats::quantile({{value_var}}, probs = 0.10, names = FALSE),
+      p25 = stats::quantile({{value_var}}, probs = 0.25, names = FALSE),
+      p75 = stats::quantile({{value_var}}, probs = 0.75, names = FALSE),
+      p90 = stats::quantile({{value_var}}, probs = 0.90, names = FALSE),
+      #p10_w = caTools::runquantile({{value_var}}, k = 5, p = 0.1)[1],
+      #p90_w = caTools::runquantile({{value_var}}, k = 5, p = 0.9)[1],
+      l_eto_3 = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal"]], b_op = "lte"),
+      l_eto_5 = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]], b_op = "lte"),
+      h_eto_3 = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal"]], b_op = "gte"),
+      h_eto_5 = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]], b_op = "gte"),
+    )
+  ) 
+}
+
---
+++ tests/testthat/test-summarise_evapotranspiration.R
@@ -0,0 +1,21 @@
+test_that("summarise_evapotrapiration works", {
+  normals <- evapotranspiration_data |>
+   dplyr::mutate(year = lubridate::year(date)) |>
+   dplyr::mutate(month = lubridate::month(date)) |>
+   dplyr::group_by(code_muni, year, month) |>
+   summarise_normal(date_var = date, value_var = value, year_start = 1961, year_end = 1990) |>
+   dplyr::ungroup()
+   
+   res <- evapotranspiration_data |>
+     dplyr::mutate(year = lubridate::year(date)) |>
+     dplyr::mutate(month = lubridate::month(date)) |>
+     dplyr::group_by(code_muni, year, month) |>
+      summarise_evapotrapiration(value_var = value, normals_df = normals) |>
+     dplyr::ungroup()
+ 
+   expect_true(all(c("l_eto_3", "h_eto_3") %in% names(res)))
+ })
+ 
+ test_that("summarise_evapotrapiration not works with ungrouped data", {
+   expect_error(summarise_evapotrapiration(var = solar_radiation_data))
+ })
