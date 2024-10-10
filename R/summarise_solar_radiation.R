3b3531975893e4c1623ab1c847d67b125779115e
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-10 14:26:34 +0200

    Use normal as reference

--- R/summarise_solar_radiation.R
+++ R/summarise_solar_radiation.R
@@ -2,6 +2,9 @@
 #'
 #' The function computes solar radiation indicators from grouped data. Expects solar radiation in MJm-2.
 #' 
+#' @details
+#' The dark and light days indicators are computed based on climatological normals, created with the `summarise_normal()` function and passed with the `normals_df` argument. Keys to join the normals data must be present (like id, year, and month)  and use the same names.
+#' 
 #' The following indicators are computed for each group.
 #' \itemize{
 #'  \item{`count` Count of data points}
@@ -15,20 +18,32 @@
 #'  \item{`p25` 25th percentile}
 #'  \item{`p75` 75th percentile}
 #'  \item{`p90` 90th percentile}
-#'  \item{`dark_3` Count of sequences of 3 days or more with solar radiation bellow 5}
-#'  \item{`dark_5` Count of sequences of 5 days or more with solar radiation bellow 5}
-#'  \item{`light_3` Count of sequences of 3 days or more with solar radiation above 25}
-#'  \item{`light_5` Count of sequences of 5 days or more with solar radiation above 25}
+#'  \item{`dark_3` Count of sequences of 3 days or more with solar radiation bellow the climatological normal}
+#'  \item{`dark_5` Count of sequences of 5 days or more with solar radiation bellow the climatological normal}
+#'  \item{`light_3` Count of sequences of 3 days or more with solar radiation above the climatological normal}
+#'  \item{`light_5` Count of sequences of 5 days or more with solar radiation above the climatological normal}
 #' }
 #' 
 #' @param .x grouped data, created with `dplyr::group_by()`
 #' @param value_var name of the variable with temperature values.
+#' @param normals_df normals data, created with `summarise_normal()`
 #'
 #' @return A tibble.
 #' @export
 #' @importFrom rlang .data
 #'
 #' @examples
+#' # Compute monthly normals
+#' normals <- solar_radiation_data |>
+#'   # Identify year
+#'   dplyr::mutate(year = lubridate::year(date)) |>
+#'   # Identify month
+#'   dplyr::mutate(month = lubridate::month(date)) |>
+#'   # Group by id variable, year and month
+#'   dplyr::group_by(code_muni, year, month) |>
+#'   summarise_normal(date_var = date, value_var = value, year_start = 1961, year_end = 1990) |>
+#'   dplyr::ungroup()
+#' 
 #' # Compute indicators
 #' solar_radiation_data |>
 #'  # Identify year
@@ -38,11 +53,11 @@
 #'  # Group by id variable, year and month
 #'  dplyr::group_by(code_muni, year, month) |>
 #'  # Compute precipitation indicators
-#'  summarise_solar_radiation(value_var = value) |>
+#'  summarise_solar_radiation(value_var = value, normals_df = normals) |>
 #'  # Ungroup
 #'  dplyr::ungroup()
 #' 
-summarise_solar_radiation <- function(.x, value_var){
+summarise_solar_radiation <- function(.x, value_var, normals_df){
   # Assertions
   checkmate::assert_data_frame(x = .x)
 
@@ -54,8 +69,10 @@ summarise_solar_radiation <- function(.x, value_var){
   # Compute indicators
   suppressMessages(
     .x |>
+    dplyr::inner_join(normals_df) |>
     dplyr::summarise(
       count = dplyr::n(),
+      normal = mean(.data[["normal"]], na.rm = TRUE),
       mean = mean({{value_var}}, na.rm = TRUE),
       median = stats::median({{value_var}}, na.rm = TRUE),
       sd = stats::sd({{value_var}}, na.rm = TRUE),
@@ -68,10 +85,10 @@ summarise_solar_radiation <- function(.x, value_var){
       p90 = stats::quantile({{value_var}}, probs = 0.90, names = FALSE),
       #p10_w = caTools::runquantile({{value_var}}, k = 5, p = 0.1)[1],
       #p90_w = caTools::runquantile({{value_var}}, k = 5, p = 0.9)[1],
-      dark_3 = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = 5, b_op = "lte"),
-      dark_5 = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = 5, b_op = "lte"),
-      light_3 = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = 25, b_op = "gte"),
-      light_5 = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = 25, b_op = "gte"),
+      dark_3 = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal"]], b_op = "lte"),
+      dark_5 = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]], b_op = "lte"),
+      light_3 = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal"]], b_op = "gte"),
+      light_5 = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]], b_op = "gte"),
     )
   ) 
 }
