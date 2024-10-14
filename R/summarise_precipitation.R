21045680c82c2411d899680f87912c29ed93e6ea
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-14 13:13:41 +0200

    Adapt to new normal standard.

--- R/summarise_precipitation.R
+++ R/summarise_precipitation.R
@@ -8,7 +8,9 @@
 #' The following indicators are computed for each group.
 #' \itemize{
 #'  \item{`count` Count of data points}
-#'  \item{`normal` Climatological normal, from `normals_df` argument}
+#'  \item{`normal_mean` Climatological normal mean, from `normals_df` argument}
+#'  \item{`normal_p10` Climatological 10th percentile, from `normals_df` argument}
+#'  \item{`normal_p90` Climatological 90th percentile, from `normals_df` argument}
 #'  \item{`mean` Average}
 #'  \item{`median` Median}
 #'  \item{`sd` Standard deviation}
@@ -19,8 +21,8 @@
 #'  \item{`p25` 25th percentile}
 #'  \item{`p75` 75th percentile}
 #'  \item{`p90` 90th percentile}
-#'  \item{`rain_spells_3d` Count of rain spells occurences, with 3 or more consecutive days with rain above the climatological normal value}
-#'  \item{`rain_spells_5d` Count of rain spells occurences, with 5 or more consecutive days with rain above the climatological normal value}
+#'  \item{`rain_spells_3d` Count of rain spells occurences, with 3 or more consecutive days with rain above the climatological normal average value}
+#'  \item{`rain_spells_5d` Count of rain spells occurences, with 5 or more consecutive days with rain above the climatological normal average value}
 #'  \item{`p_1` Count of days with precipitation above 1mm}
 #'  \item{`p_5` Count of days with precipitation above 5mm}
 #'  \item{`p_10` Count of days with precipitation above 10mm}
@@ -82,7 +84,9 @@ summarise_precipitation <- function(.x, value_var, normals_df){
     dplyr::inner_join(normals_df) |>
     dplyr::summarise(
       count = dplyr::n(),
-      normal = mean(.data[["normal"]], na.rm = TRUE),
+      normal_mean = head(.data[["normal_mean"]], 1),
+      normal_p10 = head(.data[["normal_p10"]], 1),
+      normal_p90 = head(.data[["normal_p90"]], 1),
       mean = mean({{value_var}}, na.rm = TRUE),
       median = stats::median({{value_var}}, na.rm = TRUE),
       sd = stats::sd({{value_var}}, na.rm = TRUE),
@@ -95,8 +99,8 @@ summarise_precipitation <- function(.x, value_var, normals_df){
       p90 = stats::quantile({{value_var}}, probs = 0.90, names = FALSE),
       #p10_w = caTools::runquantile({{value_var}}, k = 5, p = 0.1)[1],
       #p90_w = caTools::runquantile({{value_var}}, k = 5, p = 0.9)[1],
-      rain_spells_3d = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal"]], b_op = "gte"),
-      rain_spells_5d = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]], b_op = "gte"),
+      rain_spells_3d = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal_mean"]], b_op = "gte"),
+      rain_spells_5d = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal_mean"]], b_op = "gte"),
       p_1 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 1, b_op = "gte"),
       p_5 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 5, b_op = "gte"),
       p_10 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 10, b_op = "gte"),
