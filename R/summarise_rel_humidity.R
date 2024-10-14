98619c29a34a57284183f53c02a51ad1f22fe278
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-14 13:11:48 +0200

    Adapt to new normal standard.

--- R/summarise_rel_humidity.R
+++ R/summarise_rel_humidity.R
@@ -8,7 +8,9 @@
 #' The following indicators are computed for each group.
 #' \itemize{
 #'  \item{`count` Count of data points}
-#'  \item{`normal` Climatological normal, from `normals_df` argument}
+#'  \item{`normal_mean` Climatological normal mean, from `normals_df` argument}
+#'  \item{`normal_p10` Climatological 10th percentile, from `normals_df` argument}
+#'  \item{`normal_p90` Climatological 90th percentile, from `normals_df` argument}
 #'  \item{`mean` Average}
 #'  \item{`median` Median}
 #'  \item{`sd` Standard deviation}
@@ -23,8 +25,8 @@
 #'  \item{`dry_spells_5d` Count of dry spells occurences, with 5 or more consecutive days with relative humidity bellow the climatological normal value minus 10 percent}
 #'  \item{`wet_spells_3d` Count of wet spells occurences, with 3 or more consecutive days with relative humidity above the climatological normal value plus 10 percent}
 #'  \item{`wet_spells_5d` Count of wet spells occurences, with 5 or more consecutive days with relative humidity above the climatological normal value plus 10 percent}
-#'  \item{`dry_days` Count of dry days, when the relative humidity is bellow the 10th percentile}
-#'  \item{`wet_days` Count of wet days, when the relative humidity is above the 90th percentile}
+#'  \item{`dry_days` Count of dry days, when the relative humidity is bellow the normal 10th percentile}
+#'  \item{`wet_days` Count of wet days, when the relative humidity is above the normal 90th percentile}
 #'  \item{`h_21_30` Count of days with relative humidity between 21% and 30%. Attention level}
 #'  \item{`h_12_20` Count of days with relative humidity between 12% and 20%. Alert level}
 #'  \item{`h_11` Count of days with relative humidity bellow 12%. Emergence level}
@@ -78,7 +80,9 @@ summarise_rel_humidity <- function(.x, value_var, normals_df){
     dplyr::inner_join(normals_df) |>
     dplyr::summarise(
       count = dplyr::n(),
-      normal = mean(.data[["normal"]], na.rm = TRUE),
+      normal_mean = head(.data[["normal_mean"]], 1),
+      normal_p10 = head(.data[["normal_p10"]], 1),
+      normal_p90 = head(.data[["normal_p90"]], 1),
       mean = mean({{value_var}}, na.rm = TRUE),
       median = stats::median({{value_var}}, na.rm = TRUE),
       sd = stats::sd({{value_var}}, na.rm = TRUE),
@@ -91,12 +95,12 @@ summarise_rel_humidity <- function(.x, value_var, normals_df){
       p90 = stats::quantile({{value_var}}, probs = 0.90, names = FALSE),
       #p10_w = caTools::runquantile({{value_var}}, k = 5, p = 0.1)[1],
       #p90_w = caTools::runquantile({{value_var}}, k = 5, p = 0.9)[1],
-      dry_spells_3d = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal"]] - 10, b_op = "lte"),
-      dry_spells_5d = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]] - 10, b_op = "lte"),
-      wet_spells_3d = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal"]] + 10, b_op = "gte"),
-      wet_spells_5d = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal"]] + 10, b_op = "gte"),
-      dry_days = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = .data[["p10"]], b_op = "lte"),
-      wet_days = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = .data[["p90"]], b_op = "gte"),
+      dry_spells_3d = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal_mean"]] - 10, b_op = "lte"),
+      dry_spells_5d = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal_mean"]] - 10, b_op = "lte"),
+      wet_spells_3d = nseq::trle_cond(x = {{value_var}}, a = 3, a_op = "gte", b = .data[["normal_mean"]] + 10, b_op = "gte"),
+      wet_spells_5d = nseq::trle_cond(x = {{value_var}}, a = 5, a_op = "gte", b = .data[["normal_mean"]] + 10, b_op = "gte"),
+      dry_days = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = .data[["normal_p10"]], b_op = "lte"),
+      wet_days = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = .data[["normal_p90"]], b_op = "gte"),
       h_30 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 30, b_op = "lte"),
       h_20 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 20, b_op = "lte"),
       h_12 = nseq::trle_cond(x = {{value_var}}, a = 1, a_op = "gte", b = 12, b_op = "lte"),
